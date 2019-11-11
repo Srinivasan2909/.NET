f872a01d5994b2473bd39a143f395e3457fde27c
Srinivasan2909 <55695789+Srinivasan2909@users.noreply.github.com>
2019-11-11 11:28:09 +0530

    Countrytatedropdown

---
+++ countryassingment/dropdown/Controllers/dropController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dropdown.Models;
+
+namespace dropdown.Controllers
+{
+    public class dropController : Controller
+    {
+        // GET: drop
+        public ActionResult Index()
+        {
+            assingmentEntities obj = new assingmentEntities();
+            ViewBag.Countries = new SelectList(GetCountries(), "CountryID", "CountryName");
+            return View();
+        }
+        public List<country>GetCountries()
+        {
+            assingmentEntities obj = new assingmentEntities();
+            List<country> countries = obj.countries.ToList();
+            return countries;
+        }
+        public ActionResult GetStates(int CountryID)
+        {
+            assingmentEntities obj = new assingmentEntities();
+            List<state> states1 = obj.states.Where(x => x.countryid == CountryID).ToList();
+            ViewBag.abcd = new SelectList(states1, "stateid", "statename");
+            return PartialView("DisplayStates");
+        }
+    }
+}
---
+++ countryassingment/dropdown/Models/Cascading.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dropdown.Models
+{
+    public class Cascading
+    {
+        public int CountryID { get; set; }
+        public int StateID { get; set; }
+    }
+}
